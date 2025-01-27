054f1e2f66b3106dd26624631d368e34d85f4548
PANKAJ KUMAR <pankajkr.vitj@gmail.com>
2025-01-27 15:38:53 +0530

    learn little bit about array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,19 @@
+const myarr=[1,2,3,4,5,6,7,8,9,10];
+const myHeroes=['superman','batman','spiderman','ironman','thor','hulk','captain'];
+
+const myArr2=new Array(1,2,3,4,5);
+console.log(myArr2);
+//output: [1,2,3,4,5]
+console.log(myHeroes);
+//output: ['superman','batman','spiderman','ironman','thor','hulk','captain']
+console.log(myHeroes[0]);
+//output: superman
+console.log(myarr[9]);
+//output: 10
+console.log(myarr[10]);
+//output: undefined
+console.log(myarr.length);
+//output: 10
+
+// Array Methods
+
