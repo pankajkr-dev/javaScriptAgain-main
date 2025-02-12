f9d0bda74f4dbedbb47e82bde26af9b26aa36b5c
PANKAJ KUMAR <pankajkr.vitj@gmail.com>
2025-02-12 12:56:01 +0530

    learn some array methods

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -17,3 +17,44 @@ console.log(myarr.length);
 
 // Array Methods
 
+const fruints=["banana", "Orange", "Apple", "Mango"]
+let size = fruints.length;
+console.log(size);
+//output: 4
+
+const fruits=["banana", "Orange", "Apple", "Mango"]
+let fruitsString = fruits.toString();
+console.log(fruitsString);
+//output: banana,Orange,Apple,Mango
+
+const fruitss=["banana", "Orange", "Apple", "Mango"]
+let at=fruitss.at(2);
+console.log(at);
+//output: Apple
+
+const accessElement=["banana", "Orange", "Apple", "Mango"]
+let element=accessElement[1];
+console.log(element);
+//output: Orange
+
+const join=["banana", "Orange", "Apple", "Mango"]
+let joinElement=join.join("***");
+console.log(joinElement);
+//output: banana***Orange***Apple***Mango
+
+const pop=["banana", "Orange", "Apple", "Mango"]  
+let popelement=pop.pop()
+console.log(popelement);
+//output: banbna, orange, apple
+
+const push=["watermellon", "Apple", "Mango"]
+let pushElement=push.push("kiwi")
+console.log(pushElement);
+
+
+
+
+
+
+
+
